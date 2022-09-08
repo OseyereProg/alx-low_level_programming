4d13ef20928f766faa825e730496d3a4f50e607f
Oseyere <esekhaigbe.oseyere@gmail.com>
2022-09-08 12:54:26 -0700

    come

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 
 /**
- *
- * main - print
- *
- *
+ * main - print a puzzle
  *
  * Return: Always 0 (Success)
- *
  */
 
 int main(void)
 
 {
-      puts("\"Programming is like building a multilingual puzzle" );
-
+      puts("\"Programming is like building a multilingual
+ puzzle");
 	 return (0);
 
 }
