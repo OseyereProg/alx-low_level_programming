c4ef37f71edf89463b666ee089b89f8eab6289cc
Oseyere <esekhaigbe.oseyere@gmail.com>
2022-09-08 12:42:34 -0700

    3-5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ * main - print
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+      puts("\"Programming is like building a multilingual puzzle" ):
+
+	 return (0);
+
+}
