c175c680e905c8fb4488a270fa1c2ab1527f71c5
Oseyere <esekhaigbe.oseyere@gmail.com>
2022-09-09 14:34:48 -0700

    erygrre

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,10 +10,9 @@ int main(void)
 {
 int n;
 char last[] = "Last difit of ";
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("%s %d is %d and is ", last, n, n % 10);
+	printf("%s %d is %d and is ", last, n, n % 10);
 
 	if (n % 10 > 5)
 	{
