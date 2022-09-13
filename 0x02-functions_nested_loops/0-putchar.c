ad69b4968a47f56707ada26a2d4d8f74ce6b6ae3
Oseyere <esekhaigbe.oseyere@gmail.com>
2022-09-13 06:37:33 -0700

    one of them

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>                                                                                                            
+#include <time.h>                                                                                                               
+#include <stdio.h>                                                                                                              
+/*                                                                                                                             
+ *
+ * main - Prints if number is positive, zero or negative                                                                         *
+ * Return: Always (Success)                                                                                                     
+ *
+ */                                                                                                                             
+
+int main(void)                                                                                                                  
+
+{                                                                                                                               
+
+	char c = "_putchar";                                                                                                                                                                   
+                                                                                                              
+
+	while (c)
+	{		
+
+	                                                                                                                       
+		putchar(c);
+		c++;
+
+	}                                                                                                                       
+
+        putchar('\n');
+
+	return (0);  
+}					
