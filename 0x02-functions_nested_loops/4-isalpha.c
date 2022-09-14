33a26c2c4428f2d6598ff6ab5e0444b820ec0dde
Oseyere <esekhaigbe.oseyere@gmail.com>
2022-09-14 03:45:45 -0700

    agshdhd

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _isalpha - a function that checks for lowercase
+* character
+* @c: single character input
+* Return: 1 if int c is lowercase, 0 if otherwise
+* check
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
