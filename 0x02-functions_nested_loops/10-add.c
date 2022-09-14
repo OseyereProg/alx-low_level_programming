7afec8ba0ac9dc292b9de3826e6f7de218d0e82a
Oseyere <esekhaigbe.oseyere@gmail.com>
2022-09-14 13:43:52 -0700

    svsvgd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - Adds two integers.
+* @num1: The first number to be added
+* @num2: The second integer to be added
+* Return: 1 if int c is lowercase, 0 if otherwise
+* check
+*/
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
