fa697591b4169dcecd6ca2354510ce10a9dfe2ea
Oseyere <esekhaigbe.oseyere@gmail.com>
2022-09-15 13:47:54 -0700

    sgsgg

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,22 +2,18 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- *
  * Return: 0
- *
  */
 
 int main(void)
 
 {
 
-	int i;
+int i;
 
+i = 0;
 
-
-	i = 0;
-
-	positive_or_negative(i);
+positive_or_negative(i);
 
 
 
