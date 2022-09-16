bdf310816250bdd3de26648ba606113f0f14b6cd
Oseyere <esekhaigbe.oseyere@gmail.com>
2022-09-16 14:49:26 -0700

    dhdhdhd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - checks if a  character is digit or not
+ * @a: first number
+ * @b: second number
+ * Return: 1 whether it is, 0 otherwise
+ *
+ */
+
+
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
